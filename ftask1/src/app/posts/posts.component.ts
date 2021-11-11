fccffdc5f9a50cac8f4ae9f19299e6320c3352d9
skiesluv <83613911+skiesluv@users.noreply.github.com>
2021-11-11 15:43:27 +0200

    Angular Project Part 1

---
+++ ftask1/src/app/posts/posts.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService, Post } from './data.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+
+export class PostComponent implements OnInit{
+  showImage: boolean;
+  data: Post[] = [];
+  columnsToDisplay = ['userId', 'id', 'title', 'body']
+
+  constructor(private ds: DataService) {
+    this.showImage = false;
+    this.ds.getData().subscribe(x => {
+      this.data = x;
+      console.log(this.data)
+    })
+  }
+  ngOnInit(): void {};
+}
