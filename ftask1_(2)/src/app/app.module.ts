eba8a981a210c20f74820dc16e5488a677f944a7
skiesluv <83613911+skiesluv@users.noreply.github.com>
2021-11-15 03:17:38 +0200

    Angular Project Part 2

---
+++ ftask1_(2)/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostComponent } from './posts/posts.component';
+import { DataService } from './posts/data.service';
+import { HttpInterceptProvides } from './http-interceptors';
+import { FormComponent } from './form/form.component';
+import { FormUserService } from './form/form.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [DataService, HttpInterceptProvides, FormUserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
